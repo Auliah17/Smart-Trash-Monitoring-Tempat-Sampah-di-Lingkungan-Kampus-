d2f426a277afe9ed303849d623f241269eb52c23
qhayy <saharaputriqhaylha@gmail.com>
2025-12-14 19:41:47 +0800

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'welcome_page.dart';
+
+/// Biar sesuai nama file yang kamu kirim, HomePage di sini
+/// pakai layout WelcomePage (landing + jenis sampah).
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) => const WelcomePage();
+}
